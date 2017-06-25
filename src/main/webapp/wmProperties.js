1aeabef0682e519d5b0ff7703f84db923565f351
root <root@ca445d4f2acb>
2017-06-25 18:21:24 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_raXlGnvvRw",
+  "homePage" : "Main",
+  "name" : "Auto_raXlGnvvRw",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
